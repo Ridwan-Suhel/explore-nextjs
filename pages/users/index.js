e6a8b4688c850a63785b72bfe32ecf9e1521c76a
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-29 22:27:57 +0600

    dynamic routes added in next js

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const index = ({ users }) => {
   return (
     <div>
       <h2>This is from users index page - Total users: {users.length}</h2>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h2>Name: {user.name}</h2>
+          <Link href={`users/${user.id}`}>
+            <button>User</button>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
